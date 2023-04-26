bed7191f298eeff4108942e192a3119d220bfdf5
dmalitesta <d.malitesta@gmail.com>
2023-04-26 15:34:03 +0200

    Submission RecSys 2023 (Reproducibility track).

--- start_experiments_user_groups.py
+++ start_experiments_user_groups.py
@@ -2,8 +2,7 @@ from elliot.run import run_experiment
 import argparse
 
 parser = argparse.ArgumentParser(description="Run sample main.")
-parser.add_argument('--dataset', type=str, default='gowalla')
-parser.add_argument('--model', type=str, default='ngcf')
+parser.add_argument('--dataset', type=str, default='allrecipes')
 args = parser.parse_args()
 
 run_experiment(f"config_files/{args.model}_{args.dataset}.yml")
