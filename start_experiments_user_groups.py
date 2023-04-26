2888da417cd6fff7109fe91551e527dac9970ae3
dmalitesta <d.malitesta@gmail.com>
2023-04-26 15:39:56 +0200

    Submission RecSys 2023 (Reproducibility track).

--- start_experiments_user_groups.py
+++ start_experiments_user_groups.py
@@ -3,6 +3,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Run sample main.")
 parser.add_argument('--dataset', type=str, default='allrecipes')
+parser.add_argument('--hop', type=int, default=0)
 args = parser.parse_args()
 
-run_experiment(f"config_files/{args.model}_{args.dataset}.yml")
+run_experiment(f"config_files/{args.dataset}_quartiles_{args.hop}.yml")
