e22126261f6cc61bd061e4fdef6b6af23f814b3b
dmalitesta <d.malitesta@gmail.com>
2023-11-28 10:43:58 +0100

    Added non-reproducible LightGCN (LoG 2023).

--- external/models/lightgcn/LightGCN.py
+++ external/models/lightgcn/LightGCN.py
@@ -110,12 +110,6 @@ class LightGCN(RecMixin, BaseRecommenderModel):
 
             self.evaluate(it, loss / (it + 1))
 
-        if it + 1 == self._epochs and self._write_best_iterations:  # never met an early stopping condition
-            with open(self._config.path_output_rec_performance + '/best_iterations.tsv', 'a') as f:
-                f.write(self.name + '\t' + str(self._params.best_iteration) + '\n')
-            self.logger.info(f"Best iteration: {self._params.best_iteration}")
-            self.logger.info(f"Current configuration: {self.name}")
-
     def get_recommendations(self, k: int = 100):
         predictions_top_k_test = {}
         predictions_top_k_val = {}
--- external/models/sgl/SGL.py
+++ external/models/sgl/SGL.py
@@ -143,12 +143,6 @@ class SGL(RecMixin, BaseRecommenderModel):
 
             self.evaluate(it, loss / (it + 1))
 
-        if it + 1 == self._epochs and self._write_best_iterations:  # never met an early stopping condition
-            with open(self._config.path_output_rec_performance + '/best_iterations.tsv', 'a') as f:
-                f.write(self.name + '\t' + str(self._params.best_iteration) + '\n')
-            self.logger.info(f"Best iteration: {self._params.best_iteration}")
-            self.logger.info(f"Current configuration: {self.name}")
-
     def create_adj_mat(self, is_subgraph=False, aug_type='ed'):
         if is_subgraph and self._ssl_ratio > 0:
             if aug_type == 'nd':
